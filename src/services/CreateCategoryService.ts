96fa21a1876f8c8fc407e95a2056946813483b38
Rafael Cavallin <rafael_cavallin@hotmail.com>
2022-04-05 12:18:59 -0300

    Atualizando repositório remoto.

--- src/services/CreateCategoryService.ts
+++ src/services/CreateCategoryService.ts
@@ -1,4 +1,4 @@
-import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
 
 interface IRequest {
     name: string;
@@ -7,7 +7,7 @@ interface IRequest {
 
 class CreateCategoryService {
     // eslint-disable-next-line prettier/prettier
-    constructor(private categoriesRepository: CategoriesRepository) { }
+    constructor(private categoriesRepository: ICategoriesRepository) { }
 
     execute({ name, description }: IRequest): void {
         const categoryAlreadyExists =
