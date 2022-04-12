76c122d1f98bccfde6517303a549a676499ad541
Rafael Cavallin <rafael_cavallin@hotmail.com>
2022-04-12 18:02:36 -0300

    Iniciando a impntegração com banco de dados Postgres.

---
+++ src/database/migrations/1649787700519-CreateCategories.ts
@@ -0,0 +1,35 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+import { Table } from "typeorm/schema-builder/table/Table";
+
+export class CreateCategories1649787700519 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "categories",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                    },
+                    {
+                        name: "name",
+                        type: "varchar",
+                    },
+                    {
+                        name: "description",
+                        type: "varchar",
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                ],
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("categories");
+    }
+}
